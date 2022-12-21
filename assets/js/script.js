0394421720c1d71ff508a19ed1b8c5a1f2dbc398
Raul <raulmayas20@gmail.com>
2022-12-21 17:59:38 -0600

    Display Question Per Seconds

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,11 @@ var startBtn = document.getElementById("start-quiz-button");
 var mainImage = document.querySelector("img");
 var h1Text = document.querySelector("h1");
 var mainSection = document.querySelector("section");
+var timeEl = document.createElement("p");
+var h2Text = document.createElement("h2");
+var listEl = document.createElement("ul");
+
+mainSection.append(timeEl);
 
 var questions = [
   questionOne = {
@@ -17,26 +22,46 @@ var questions = [
     question: "What country does Aubameyang play for?",
     answers: ["Gabón", "Etiophia", "Egypt", "Guinea"],
   },
+  questionThree = {
+    question: "Who score the only goal of the EURO 2016 final? ",
+    answers: ["Cristiano Ronaldo", "Eder", "Renato Sanches", "Rafael Guerreiro"],
+  },
+  questionFour = {
+    question: "Who won the Golden Ball of South Africa 2010 World Cup",
+    answers: ["Lionel Messi", "Andres Iniesta", "Diego Forlan", "Wesley Sneijder"],
+  },
+  questionFive = {
+    question: "In the 2014 UCL final Real Madrid vs Atletico de Madrid, in what minute of added time did Sergio Ramos tie the game?",
+    answers: ["91", "95", "92", "93"],
+  },
+  questionSix = {
+    question: "Who won the UCL 96-97?",
+    answers: ["Juventus", "Borussia Dortmund", "FC Porto", "Ajax FC"],
+  },
+  questionSeven = {
+    question: "Which player didn’t play for Manchester United:",
+    answers: ["Troy Deeney", "Wilfred Zaha", "Zlatan Ibrahimovic", "Thomas Cleverly"],
+  },
 ];
 
 function countdown() {
-  var timeLeft = 5;
-  var timeEl = document.createElement("p");
+  var timeLeft = 2;
+  
   // TODO: Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
   var timeInterval = setInterval(function () {
     // YOUR CODE HERE
     if (timeLeft > 1) {
       timeEl.textContent = timeLeft + " Seconds Left.";
       timeLeft--;
-      mainSection.append(timeEl);
+
     } else if (timeLeft === 1) {
       timeEl.textContent = timeLeft + " Second Left.";
       timeLeft--;
-      mainSection.append(timeEl);
+      
     } else {
-      timeEl.textContent = "";
-      mainSection.append(timeEl);
       clearInterval(timeInterval);
+      diplayQuestion();
+      
     }
   }, 1000); // 1000 milliseconds
 }
@@ -45,15 +70,15 @@ function startQuiz() {
   startBtn.setAttribute("class", "hidden");
   mainImage.setAttribute("class", "hidden");
   h1Text.setAttribute("class", "hidden");
-  countdown();
   diplayQuestion();
 }
 
 function diplayQuestion() {
-  var h2Text = document.createElement("h2");
-  var listEl = document.createElement("ul");
-  h2Text.textContent = questions[0].question
-  for (item of questions[0].answers) {
+  countdown();
+  var arrayindex =   Math.round(Math.random() * questions.length)
+  h2Text.textContent = questions[arrayindex].question
+  listEl.textContent = ""
+  for (item of questions[arrayindex].answers) {
     var listItem =  document.createElement("li");
     listItem.textContent = item;
     listEl.append(listItem);
