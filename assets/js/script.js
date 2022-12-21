abad75ed4479ef8ddeeeac06f15e1022fd27e1d5
Raul <raulmayas20@gmail.com>
2022-12-21 15:53:40 -0600

    Displaying Timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,40 +2,50 @@ var startBtn = document.getElementById("start-quiz-button");
 var mainImage = document.querySelector("img");
 var h1Text = document.querySelector("h1");
 var mainSection = document.querySelector("section");
-var myQuestion = document.createElement("h2");
-var answerContainer = document.createElement("ul");
 
-mainSection.appendChild(myQuestion);
-mainSection.appendChild(answerContainer)
-
-
-var questions = {
-  questionOne: {
+var questions = [
+  questionOne = {
     question: "Who has scored the most goals in World Cup competitions?",
-    answerOne: "David Villa",
-    answerTwo: "Ronaldo Nazario",
-    answerThree: "Gary Lineker",
-    answerFour: "Miroslav Klose",
+    answers: [
+      "David Villa",
+      "Ronaldo Nazario",
+      "Gary Lineker",
+      "Miroslav Klose",
+    ],
   },
-  questionTwo: {
+  questionTwo = {
     question: "What country does Aubameyang play for?",
-    answerOne: "Gabón",
-    answerTwo: "Etiophia",
-    answerThree: "Egypt",
-    answerFour: "Guinea",
+    answers: ["Gabón", "Etiophia", "Egypt", "Guinea"],
   },
-};
+];
 
+function countdown() {
+  var timeLeft = 5;
+  // TODO: Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+  var timeInterval = setInterval(function () {
+    // YOUR CODE HERE
+    if (timeLeft > 1) {
+      mainSection.textContent = timeLeft + " Seconds Left.";
+      timeLeft--;
+    } else if (timeLeft === 1) {
+      mainSection.textContent = timeLeft + " Second Left.";
+      timeLeft--;
+    } else {
+      mainSection.textContent = "";
+      clearInterval(timeInterval);
+    }
+  }, 1000); // 1000 milliseconds
+}
 
 function startQuiz() {
   startBtn.setAttribute("class", "hidden");
   mainImage.setAttribute("class", "hidden");
   h1Text.setAttribute("class", "hidden");
+  countdown();
 }
 
-function loadQuestions() {
+function diplayQuestion() {
   
 }
 
 startBtn.addEventListener("click", startQuiz);
-
