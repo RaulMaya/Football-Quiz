18fc65273e0ce9d9da1f160dbc0e2f009ae85f0b
Raul <raulmayas20@gmail.com>
2022-12-21 16:16:34 -0600

    Displaying questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,17 +21,21 @@ var questions = [
 
 function countdown() {
   var timeLeft = 5;
+  var timeEl = document.createElement("p");
   // TODO: Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
   var timeInterval = setInterval(function () {
     // YOUR CODE HERE
     if (timeLeft > 1) {
-      mainSection.textContent = timeLeft + " Seconds Left.";
+      timeEl.textContent = timeLeft + " Seconds Left.";
       timeLeft--;
+      mainSection.append(timeEl);
     } else if (timeLeft === 1) {
-      mainSection.textContent = timeLeft + " Second Left.";
+      timeEl.textContent = timeLeft + " Second Left.";
       timeLeft--;
+      mainSection.append(timeEl);
     } else {
-      mainSection.textContent = "";
+      timeEl.textContent = "";
+      mainSection.append(timeEl);
       clearInterval(timeInterval);
     }
   }, 1000); // 1000 milliseconds
@@ -42,10 +46,20 @@ function startQuiz() {
   mainImage.setAttribute("class", "hidden");
   h1Text.setAttribute("class", "hidden");
   countdown();
+  diplayQuestion();
 }
 
 function diplayQuestion() {
-  
+  var h2Text = document.createElement("h2");
+  var listEl = document.createElement("ul");
+  h2Text.textContent = questions[0].question
+  for (item of questions[0].answers) {
+    var listItem =  document.createElement("li");
+    listItem.textContent = item;
+    listEl.append(listItem);
+  }
+  mainSection.append(h2Text);
+  mainSection.append(listEl);
 }
 
 startBtn.addEventListener("click", startQuiz);
