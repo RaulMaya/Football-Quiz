df0313c9f0406018ff170d23c98d6304e7d9c94a
Raul <raulmayas20@gmail.com>
2023-01-03 12:25:45 -0600

    Give events to the answer buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,37 +22,37 @@ var questions = [
   questionTwo = {
     question: "What country does Aubameyang play for?",
     answers: ["Gabón", "Etiophia", "Egypt", "Guinea"],
-    correctAnswer: "Miroslav Klose",
+    correctAnswer: "Gabón",
   },
   questionThree = {
     question: "Who score the only goal of the EURO 2016 final? ",
-    answers: ["Cristiano Ronaldo", "Eder", "Renato Sanches", "Rafael Guerreiro"],
-    correctAnswer: "Miroslav Klose",
+    answers: ["Cristiano Ronaldo", "Ederzito António", "Renato Sanches", "Rafael Guerreiro"],
+    correctAnswer: "Ederzito António",
   },
   questionFour = {
     question: "Who won the Golden Ball of South Africa 2010 World Cup",
     answers: ["Lionel Messi", "Andres Iniesta", "Diego Forlan", "Wesley Sneijder"],
-    correctAnswer: "Miroslav Klose",
+    correctAnswer: "Diego Forlan",
   },
   questionFive = {
     question: "In the 2014 UCL final Real Madrid vs Atletico de Madrid, in what minute of added time did Sergio Ramos tie the game?",
     answers: ["91", "95", "92", "93"],
-    correctAnswer: "Miroslav Klose",
+    correctAnswer: "93",
   },
   questionSix = {
     question: "Who won the UCL 96-97?",
     answers: ["Juventus", "Borussia Dortmund", "FC Porto", "Ajax FC"],
-    correctAnswer: "Miroslav Klose",
+    correctAnswer: "Borussia Dortmund",
   },
   questionSeven = {
     question: "Which player didn’t play for Manchester United:",
     answers: ["Troy Deeney", "Wilfred Zaha", "Zlatan Ibrahimovic", "Thomas Cleverly"],
-    correctAnswer: "Miroslav Klose",
+    correctAnswer: "Troy Deeney",
   },
 ];
 
 function countdown() {
-  var timeLeft = 2;
+  var timeLeft = 120;
   
   // TODO: Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
   var timeInterval = setInterval(function () {
@@ -82,19 +82,41 @@ function startQuiz() {
 
 function diplayQuestion() {
   countdown();
-  var arrayindex =   Math.round(Math.random() * questions.length)
+  var arrayindex = Math.round(Math.random() * questions.length)
   h2Text.textContent = questions[arrayindex].question
   listEl.textContent = ""
   for (item of questions[arrayindex].answers) {
     console.log(item)
     var listItem =  document.createElement("li");
     var answerBtn =  document.createElement("button");
+    answerBtn.setAttribute("class", "multipleChoice")
+    answerBtn.setAttribute("id", item)
     answerBtn.textContent = item;
     listEl.append(listItem);
     listItem.append(answerBtn);
   }
   mainSection.append(h2Text);
   mainSection.append(listEl);
+
+  var answersList = document.getElementsByClassName("multipleChoice");
+  console.log(answersList);
+  answersList[0].addEventListener("click", function () {
+    console.log(this)
+    console.log(questions[arrayindex].correctAnswer)
+  })
+  answersList[1].addEventListener("click", function () {
+    console.log(this)
+    console.log(questions[arrayindex].correctAnswer)
+  })
+  answersList[2].addEventListener("click", function () {
+    console.log(this)
+    console.log(questions[arrayindex].correctAnswer)
+  })
+  answersList[3].addEventListener("click", function () {
+    console.log(this)
+    console.log(questions[arrayindex].correctAnswer)
+  })
+
 }
 
 startBtn.addEventListener("click", startQuiz);
